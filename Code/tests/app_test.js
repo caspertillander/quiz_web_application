cb38e3cdf71b2bb2e98d858f0a9739c4bf68e53a
caspertillander <142217827+caspertillander@users.noreply.github.com>
2023-11-02 22:20:55 +0200

    Add files via upload

---
+++ Code/tests/app_test.js
@@ -0,0 +1,6 @@
+import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
+import { app } from "../app.js";
+import { assertEquals } from "https://deno.land/std@0.171.0/testing/asserts.ts";
+import * as statisticsService from "../services/statisticsService.js";
+
+
